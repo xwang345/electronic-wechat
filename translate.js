73dcb32ff45cc1dbce3a82566f9368d0ef4557fe
Xiaochen Wang <ryan.wang23@hotmail.com>
2018-10-11 21:48:29 -0400

    first commit

---
+++ translate.js
@@ -0,0 +1,14 @@
+var arrLang = {
+    'en':{
+        selList: 'Play List',
+        myList: 'My List',
+        search: 'Search',
+        about: 'About'
+    },
+    'ch':{
+        selList: '精选歌单',
+        myList: '我的歌单',
+        search: '快速搜索',
+        about:''
+    }
+};
